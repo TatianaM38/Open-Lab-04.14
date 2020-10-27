3abba92ec5d21bda9ad1a80e91f7b026c6e1d846
tmichalikova231@gmail.com <tmichalikova231@gmail.com>
2020-10-27 21:12:22 +0100

    coffee is my savior

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,38 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            string frame = "";
+            int idk = 0;
+
+            foreach (string smth in strings)
+            {
+                if (idk < smth.Length)
+                    idk = smth.Length;
+            }
+
+            for (int i = 0; i < idk + 4; i++)
+            {
+                frame += "*";
+            }
+
+            Console.WriteLine(frame);
+
+            foreach (string smth in strings)
+            {
+                if (smth.Length < idk)
+                {
+                    string str = "";
+                    for (int i = 0; i < idk - smth.Length; i++)
+                    {
+                        str += " ";
+                    }
+                    Console.WriteLine("* " + smth + str + " *");
+                }
+                else
+                    Console.WriteLine("* " + smth + " *");
+            }
+
+            Console.WriteLine(frame);
         }
     }
 }
